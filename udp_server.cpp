51b648e3b10dc1710e3ff22b375a8f5861fd6848
MertAdak <mert.adak@ozu.edu.tr>
2022-06-29 11:05:11 +0300

    Making the code more clean and clear

--- udp_server.cpp
+++ udp_server.cpp
@@ -1,52 +1,32 @@
 #include "udp_server.h"
 
 udp_server::udp_server(int port, const char* comms_ip){
-
-    //Create socket here
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    std::cout << "socfd: " << sockfd << std::endl;
-    if(sockfd < 0){  // int sockfd = socket(domain, type, protocol) and AF_INET uses IPv4 and SOCK_DGRAM uses Datagram Protocol=UDP
-        cout<<"Cannot create socket"<<endl;
-        exit(-1);   
-    }
-    memset(&server_message, '\0', sizeof(server_message));
     memset(&client_message, '\0', sizeof(client_message));
-    cout<<"Socket created successfully!"<<endl;
-    //Identify socket here
     serv_addr.sin_family      = AF_INET;
     serv_addr.sin_port        = htons(port); //htons means convert a number into 16-bit network representation
     serv_addr.sin_addr.s_addr = inet_addr(comms_ip);
 
-        // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 ) { 
-        perror("bind failed"); 
+    //Create socket here
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sockfd < 0){  // int sockfd = socket(domain, type, protocol) and AF_INET uses IPv4 and SOCK_DGRAM uses Datagram Protocol=UDP
+        std::cout << "Cannot create socket" << std::endl;
+        exit(-1);   
+    }
+    std::cout << "Socket created successfully and ready to recieve message!" << std::endl;
+    // Bind the socket with the server address 
+    if (bind(sockfd, (const struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 ) { 
+        perror("Binding the socket failed, cannot recieve message!"); 
         exit(EXIT_FAILURE); 
-        } 
-    std::cout<<"debug"<<std::endl;
+    }
+    std::cout << "Successfully binded the socket" << std::endl;
 
 };
 void udp_server::recieve_message(){
-    while(true){
-        int n = recvfrom(sockfd, client_message, sizeof(client_message), MSG_WAITALL, (struct sockaddr*) &client_addr, &cLen);
-
-    std::cout << "n: " << n << std::endl; 
-
-    printf("Received message from IP: %s and port: %i\n",
-           inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-    
-    printf("Msg from client: %s\n", client_message);
+    if(recvfrom(sockfd, client_message, sizeof(client_message), MSG_WAITALL, (struct sockaddr*) &client_addr, &cLen)<0){
+        std::cout << "Cannot recieve the message!" << std::endl;
     }
-    
-    
-    // // Respond to client:
-    // strcpy(server_message, client_message);
-    
-    // if (sendto(sockfd, server_message, strlen(server_message), 0, (struct sockaddr*)&client_addr, cLen) < 0){
-    //     printf("Can't send\n");
-    //     exit(-1);
-    // }
-    
-    // Close the socket:
+    printf("Received message from IP: %s and port: %i\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    printf("Msg from client: %s\n", client_message);
     close(sockfd);
     exit(0);
 }
--- udp_server.h
+++ udp_server.h
@@ -15,13 +15,10 @@ class udp_server{
 public:
     int sockfd;
     char client_message[256];
-    char server_message[256];
     struct sockaddr_in client_addr, serv_addr; //server address and client address
     socklen_t cLen = sizeof(client_addr);
 
     udp_server(int port, const char* comms_ip);
     void recieve_message();
-
-
 };
 #endif
